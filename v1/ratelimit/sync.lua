40329241ae6addb30ae4aad3e3dd7701357860cd
Arthur <arthur@pintu.co.id>
2025-10-09 12:35:43 +0800

    fix: add 1 hour TTL to all keys in case clean-up never happens (server shutdown or else)

--- v1/ratelimit/sync.lua
+++ v1/ratelimit/sync.lua
@@ -20,8 +20,9 @@ local currentValue = nil
 
 -- Case 1: first sync, key not yet synced anywhere
 if not hasSyncedBefore and resetAt > 0 then
-    local wasSet = redis.call('SET', key, resetAt, 'NX')
-    if wasSet then
+    -- set expiry to 1 hour in case this key is never cleaned by the client
+    local was_set = redis.call('SET', key, resetAt, 'NX', "PX", 3600)
+    if was_set then
         -- Successfully created the key
         return { 'ok', resetAt }
     end
