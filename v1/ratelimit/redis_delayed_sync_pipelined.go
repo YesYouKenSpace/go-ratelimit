bb16ccef13dc9f620cec731241e9926e25551d99
arthhhhh23 <arthur@pintu.co.id>
2025-10-16 10:44:22 +0800

    feat: add prefix to redis keys (#34)
    * feat: add prefix to redis keys
    
    * refactor: make Redis prefix configurable

--- v1/ratelimit/redis_delayed_sync_pipelined.go
+++ v1/ratelimit/redis_delayed_sync_pipelined.go
@@ -34,6 +34,7 @@ type RedisDelayedSyncPipelined struct {
 	cancel                context.CancelFunc
 	inner                 *SyncMapLoadThenLoadOrStore[*limiter.ResetBasedLimiter]
 	redisClient           *redis.Client
+	redisPrefix           string
 	lastSyncedResetAt     sync.Map
 	syncErrorHandler      func(error)
 	keyExpiry             time.Duration
@@ -51,8 +52,10 @@ type RedisDelayedSyncPipelined struct {
 type RedisDelayedSyncPipelinedOption struct {
 	// SyncInterval is the interval to sync the rate limit to the redis
 	// Adjust this value to trade off between the performance and the accuracy of the rate limit
-	SyncInterval          time.Duration
-	RedisClient           *redis.Client
+	SyncInterval time.Duration
+	RedisClient  *redis.Client
+	// RedisPrefix prefix to add to Redis key. Default is "yyks:gort::".
+	RedisPrefix           string
 	SyncErrorHandler      func(error)
 	KeyExpiry             time.Duration
 	DisableAutoSync       bool
@@ -93,10 +96,16 @@ func NewRedisDelayedSyncPipelined(ctx context.Context, opt RedisDelayedSyncPipel
 		batchSize = DefaultBatchSize
 	}
 
+	prefix := "yyks:gort::"
+	if len(opt.RedisPrefix) > 0 {
+		prefix = opt.RedisPrefix
+	}
+
 	rl := &RedisDelayedSyncPipelined{
 		ctx:                   ctx,
 		cancel:                cancel,
 		redisClient:           opt.RedisClient,
+		redisPrefix:           prefix,
 		syncInterval:          opt.SyncInterval,
 		inner:                 NewSyncMapLoadThenLoadOrStore(limiter.NewResetbasedLimiter),
 		lastSyncedResetAt:     sync.Map{},
@@ -258,7 +267,7 @@ func (r *RedisDelayedSyncPipelined) syncAll() (err error) {
 func (r *RedisDelayedSyncPipelined) executeCorruptedRemoteRecovery(key string, limiter *limiter.ResetBasedLimiter, delta int64, lastSynced int64) error {
 	switch r.corruptedRemotePolicy {
 	case RedisDelayedSyncCorruptedRemotePolicyUploadLocal:
-		cmd := r.redisClient.Set(r.ctx, key, lastSynced, time.Hour)
+		cmd := r.redisClient.Set(r.ctx, r.prefixKey(key), lastSynced, time.Hour)
 		if cmd.Err() != nil {
 			return cmd.Err()
 		}
@@ -336,4 +345,8 @@ func (r *RedisDelayedSyncPipelined) processSyncRes(cmdArgs syncArgs, cmdRes inte
 	}
 
 	return nil
+}
+
+func (r *RedisDelayedSyncPipelined) prefixKey(key string) string {
+	return fmt.Sprintf("%s%s", r.redisPrefix, key)
 }
